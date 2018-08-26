42898b5b3ba8743dff6f9d96e7b5ce7d0248cf6b
Espen Aa Ljosland <ealv@baze.no>
2018-08-26 21:21:24 +0200

    Fixed "No trigger for successful measurement" #1

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This app brings speedtest to Homey. 
 
-Trigger speedtest "manually" via flow action, or configure a interval in the app settings page.
+Trigger speedtest "manually" via flow action, or configure an interval in the app settings page.
 
 #####Flow trigger cards:
 - Speedtest complete - tags:
@@ -10,9 +10,19 @@ Trigger speedtest "manually" via flow action, or configure a interval in the app
     - Download speed difference from last result (percentage)
     - Upload speed (mbps)
     - Upload speed difference from last result (percentage)
+- Speedtest failed
 
 #####Flow action cards:
 - Run speedtest
+
+
+## Release notes
+
+#### 0.0.2
+- Fixed "Speedtest complete" flow trigger did not go through
+
+#### 0.0.1
+- Initial version
 
 ## N.B.
 Remember! The wireless connection between your Homey and your access point is very likely to be a bottleneck, and the results from speedtest running on the Homey will not necessarily reflect the routers connection speed.
--- app.js
+++ app.js
@@ -51,22 +51,22 @@ class MyApp extends Homey.App {
 
             test.on('data', data => {
                 let result = {
-                    download: data.speeds.download.toFixed(2),
-                    upload: data.speeds.upload.toFixed(2),
+                    download: +data.speeds.download.toFixed(2),
                     downloadDiff: 0,
+                    upload: +data.speeds.upload.toFixed(2),
                     uploadDiff: 0,
-                    ping: data.server.ping.toFixed(2)
+                    ping: +data.server.ping.toFixed(2)
                 };
                 if (this._lastResult) {
                     if (this._lastResult.upload <= 0)
                         result.uploadDiff = 100;
                     else
-                        result.uploadDiff = (100 * result.upload / this._lastResult.upload - 100).toFixed(2);
+                        result.uploadDiff = +(100 * result.upload / this._lastResult.upload - 100).toFixed(2);
 
                     if (this._lastResult.download <= 0)
                         result.downloadDiff = 100;
                     else
-                        result.downloadDiff = (100 * result.download / this._lastResult.download - 100).toFixed(2);
+                        result.downloadDiff = +(100 * result.download / this._lastResult.download - 100).toFixed(2);
                 }
                 this._lastResult = result;
                 this._speedtestCompletedTrigger.trigger(result).catch(this.error);
